6e400996c9e05d146f5aba6d1d3eb01c32a59c95
Amir Hamidi <Amirarsalanh2001@gmail.com>
2022-11-03 12:48:04 +0330

    calling the index page
    Added urls
    Update admin
    Update models
    Update views
    Create urls
    Create forms
    Migrate project

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """This class is for django administration"""
+    date_hierarchy = 'created_date'
+    list_display = ('name', 'email')
+    list_filter = ('email',)
+    search_fields = ('name', 'message')
+
+admin.site.register(Contact, ContactAdmin)
--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import ContactForm
 
-# Create your views here.
+
+class IndexView(FormView):
+    """This class is for calling the index page"""
+    template_name = 'index.html'
+    form_class = ContactForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        form.save()
+        return super().form_valid(form)
