31ec2cb1989517a393fcbd7322b632756b33fac0
Amir Hamidi <Amirarsalanh2001@gmail.com>
2026-08-01 08:54:03 +0330

    style: organize imports and fix code formatting
    - Reorder imports in settings.py, urls.py, manage.py, and admin.py
    - Add blank lines for better readability and PEP 8 compliance
    - Move decouple import before django.conf in urls.py
    - Remove unnecessary blank lines in admin.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import include, path
+from decouple import config
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
-from decouple import config
-
+from django.urls import include, path
 from website.sitemaps import StaticViewSitemap
 from website.views import AdsTxtView, Custom404View
 
